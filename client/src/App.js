3d92dc053292ab271688d9940f0fa5f3a5656e96
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-17 09:28:08 -0800

    app setup and tests are running

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      apiData: []
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  componentDidMount() {
+    document.title = `You clicked ${this.state.count} times`;
+
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log(res)
+      this.setState({
+        ...this.state,
+        apiData: res.data
+      })
+    })
+    .catch(err => {
+      console.log('oh no!!!', err)
+    })
+  }
+  
+  componentDidUpdate() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>You clicked {this.state.count} times</p>
+          <button onClick={this.handleClick}>
+            Click me
+          </button>
+          <ul>
+            {this.state.apiData.map(item =>  <li>{item.name}</li>)}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
