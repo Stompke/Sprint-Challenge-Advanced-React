d7a6c405505a7590f83c9506cc1f6074e5c0d96c
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-17 09:35:24 -0800

    fixed package.json bug

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -35,32 +35,32 @@ describe('App component', () => {
 
 
 
-// let container;
+let container;
 
-// beforeEach(() => {
-//   container = document.createElement('div');
-//   document.body.appendChild(container);
-// });
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
 
-// afterEach(() => {
-//   document.body.removeChild(container);
-//   container = null;
-// });
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
-// it('can render and update a counter', () => {
-//   // Test first render and componentDidMount
-//   act(() => {
-//     ReactDOM.render(<App />, container);
-//   });
-//   const button = container.querySelector('button');
-//   const label = container.querySelector('p');
-//   expect(label.textContent).toBe('You clicked 0 times');
-//   expect(document.title).toBe('You clicked 0 times');
+it('can render and update a counter', () => {
+  // Test first render and componentDidMount
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const button = container.querySelector('button');
+  const label = container.querySelector('p');
+  expect(label.textContent).toBe('You clicked 0 times');
+  expect(document.title).toBe('You clicked 0 times');
 
-//   // Test second render and componentDidUpdate
-//   act(() => {
-//     button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//   });
-//   expect(label.textContent).toBe('You clicked 1 times');
-//   expect(document.title).toBe('You clicked 1 times');
-// });
+  // Test second render and componentDidUpdate
+  act(() => {
+    button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+  expect(label.textContent).toBe('You clicked 1 times');
+  expect(document.title).toBe('You clicked 1 times');
+});
